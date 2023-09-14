79efdd18a43e9ab43d803f7fd4bcc76ff695e480
yaozuowei <1248487360@qq.com>
2023-09-14 13:11:13 +0800

    提交1

---
+++ src/main/java/com/yzw/web/aop/test.java
@@ -0,0 +1,13 @@
+package com.yzw.web.aop;
+
+/**
+ * @author YaoZuoWei
+ * @date 2023-9-14 13:10
+ * @description:
+ */
+public class test {
+
+    public void test(){
+        System.out.println(123);
+    }
+}
